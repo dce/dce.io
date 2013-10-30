16fc463fb113d4d82c71a27e6ab929a2fb4665ba
David Eisinger <david.eisinger@gmail.com>
2013-10-29 22:42:14 -0400

    Basic URL shortener functionality

--- src/dce_io/dce_io.go
+++ src/dce_io/dce_io.go
@@ -1,15 +1,38 @@
 package main
 
 import (
-  "fmt"
+  "log"
   "net/http"
+  "github.com/garyburd/redigo/redis"
 )
 
 func handler(writer http.ResponseWriter, request *http.Request) {
-  fmt.Fprintf(writer, "dce.io")
+  conn, err := redis.Dial("tcp", ":6379")
+  path := request.URL.Path[1:]
+
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  reply, err := conn.Do("HGET", "dce_io_redirects", path)
+
+  log.Print("KEY: ", path)
+
+  if err != nil {
+    log.Fatal(err)
+  } else if reply == nil {
+    log.Print("404")
+    http.NotFound(writer, request)
+  } else {
+    destination, _ := redis.String(reply, nil)
+    log.Print("302 ", destination)
+    http.Redirect(writer, request, destination, http.StatusFound)
+  }
+
+  conn.Close()
 }
 
 func main() {
   http.HandleFunc("/", handler)
-  http.ListenAndServe(":8080", nil)
+  http.ListenAndServe(":80", nil)
 }
